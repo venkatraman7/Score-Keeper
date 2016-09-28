87d8fdea983aeacf6570c2e56ea89c8ad18c7ef4
Vignesh <vignesh_m@trimble.com>
2016-09-28 19:24:54 +0530

    Created the SourceKeeper Project. Added Document.

---
+++ Android/ScoreKeeper/src/main/java/com/collaboration/scorekeeper/ScoreKeeperActivity.java
@@ -0,0 +1,13 @@
+package com.collaboration.scorekeeper;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ScoreKeeperActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_score_keeper);
+  }
+}
